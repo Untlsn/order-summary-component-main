507c1ab4b8e9bb7e26c8f1d39842f568f18f73e7
Filip Skoczen <philipskoczen@gmail.com>
2022-01-19 15:11:45 +0100

    Create component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
+<script  lang="ts" setup>
+import illustration from '~/assets/images/illustration-hero.svg'
+import music from '~/assets/images/icon-music.svg'
+
+</script>
+
 <template>
-  <div class='flex items-center justify-center h-screen text-3xl gap-2'>
-    Hello
-    <span class="text-[#3EAF7C]">Vue3</span>
-    +
-    <span class="text-[#9B4EF2]">Vite</span>
-    +
-    <span class="text-[#41A0E5]">WindiCSS</span>
-  </div>
+  <main class="bg-blue-background text-blue-text h-screen flex items-center justify-center">
+    <article class="w-100 rounded-3xl bg-white overflow-hidden shadow-xl">
+      <img class="w-screen" :src="illustration" alt="">
+      <div class='text-center p-8 space-y-4 flex flex-col items-center'>
+        <h1 class="capitalize text-3xl font-bold text-blue-title">
+          order summary
+        </h1>
+        <p class="text-[#9498AB] px-4 text-semibold">
+          You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!
+        </p>
+        <section class="flex w-full items-center justify-between bg-blue-section rounded-xl p-4 gap-4">
+          <img :src="music" alt="">
+          <div class="flex-1 text-left">
+            <h1 class="text-blue-title font-semibold">Annual Plan</h1>
+            <p class="text-[#9498AB]">$59.99/year</p>
+          </div>
+          <button class="text-blue-native font-semibold underline hover:(no-underline text-blue-hover) focus:outline-none">Change</button>
+        </section>
+        <button class="font-semibold bg-blue-native text-white p-4 w-full rounded-xl shadow-xl shadow-blue-native hover:bg-blue-hover focus:outline-none">
+          Proceed to Payment
+        </button>
+        <button class="font-semibold hover:text-blue-title focus:outline-none">
+          Cancel Order
+        </button>
+      </div>
+    </article>
+  </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          native: '#493ACB',
+          hover: '#766CF1',
+          background: '#E0E8FF',
+          title: '#223458',
+          text: '#9498AB',
+          section: '#F8F9FE',
+        }
+      }
+    },
   },
   plugins: [],
 };
